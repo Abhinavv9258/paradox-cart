50d7c41515e675d6d8b9a0668ae7fa175a4748e2
Abhinav Verma <79451162+Abhinavv9258@users.noreply.github.com>
2023-11-16 00:06:18 +0530

    Updated App.js and Manage routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
+import * as React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// importing toast
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// importing styles
 import './App.css';
 
-function App() {
+// exporting server URL
+export const URL = process.env.REACT_APP_SERVER_URL;
+
+
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                App
-            </header>
-        </div>
+        <BrowserRouter>
+            <ToastContainer />
+            <Routes>
+                <Route path='/' element={<Index />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
