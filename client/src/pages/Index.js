74000821d7d569b2dbc43b0a705b30f4761fcc17
Abhinav Verma <79451162+Abhinavv9258@users.noreply.github.com>
2023-11-16 23:16:27 +0530

    Added Sections and navbar

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
+import { Box } from '@mui/material';
+
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Section1 from '../components/IndexContent/Section1';
+import Section2 from '../components/IndexContent/Section2';
+import Section3 from '../components/IndexContent/Section3';
+import Section4 from '../components/IndexContent/Section4';
+
+import { Element } from 'react-scroll';
+
+
 
 const Index = () => {
     return (
         <>
-
-            Index
+            <Box className='app-container'>
+                <Navbar />
+                <Element name="section1">
+                    <Section1 />
+                </Element>
+                <Element name="section2">
+                    <Section2 />
+                </Element>
+                <Element name="section3">
+                    <Section3 />
+                </Element>
+                <Element name="section4">
+                    <Section4 />
+                </Element>
+                <Footer />
+            </Box>
         </>
     );
 };
